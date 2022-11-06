89eee83c82ace3a76d271b01dcb0de9bfc2c1075
lyoung4 <lyoung4@ithaca.edu>
2022-11-06 17:50:10 -0500

    Added more dependies for Navigation

--- Navigation.js
+++ Navigation.js
@@ -1,34 +1,57 @@
 import * as React from 'react';
-import { Text, View } from 'react-native';
+import { Text, View,TouchableHighlight } from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
 
-function HomeScreen() {
+const Stack = createNativeStackNavigator();
+
+
+function HomeScreen({ navigation }) {
     return (
         <View style={{
             flex: 1, justifyContent: 'center', alignItems: 'center'
         }}>
             <Text>Home!</Text>
+            <TouchableHighlight
+                onPress={() => navigation.navigate('Details')}
+                activeOpacity={0.6}
+                underlayColor='red'
+            >
+                <Text style={styles.button}>Go To Details</Text>
+            </TouchableHighlight>
+
         </View>
+
     );
 }
-function SettingsScreen() {
+function AboutScreen() {
     return (
         <View style={{
             flex: 1, justifyContent: 'center', alignItems: 'center'
         }}>
-            <Text>Settings!</Text>
+            <Text>About!</Text>
+            <TouchableHighlight
+                onPress={() => navigation.navigate('About')}
+                activeOpacity={0.6}
+                underlayColor='red'
+            >
+                <Text style={styles.button}>Go To About!</Text>
+            </TouchableHighlight>
+
         </View>
     );
 }
 const Tab = createBottomTabNavigator();
+
 export default function Navigation() {
     return (
         <NavigationContainer>
-            <Tab.Navigator>
-                <Tab.Screen name="Home" component={HomeScreen} />
-                <Tab.Screen name="Settings" component={SettingsScreen} />
-            </Tab.Navigator>
+            <Stack.Navigator>
+                <Stack.Screen name="Home"
+                    component={HomeScreen} />
+            </Stack.Navigator>
         </NavigationContainer>
+
     );
 }
