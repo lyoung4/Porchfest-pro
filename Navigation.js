04f71ef070407d63cab013a4ae24cbbd48a2cf43
lyoung4 <lyoung4@ithaca.edu>
2022-11-06 19:38:36 -0500

    implemented stack and tab navigation

--- Navigation.js
+++ Navigation.js
@@ -1,5 +1,5 @@
 import * as React from 'react';
-import { Text, View,TouchableHighlight } from 'react-native';
+import { Text, View, TouchableHighlight } from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
@@ -7,7 +7,7 @@ import Styles from './Styles.js';
 
 
 const Stack = createNativeStackNavigator();
-
+const Tab = createBottomTabNavigator();
 
 function HomeScreen({ navigation }) {
     return (
@@ -15,8 +15,11 @@ function HomeScreen({ navigation }) {
             flex: 1, justifyContent: 'center', alignItems: 'center'
         }}>
             <Text>Home!</Text>
+
+
+
             <TouchableHighlight
-                onPress={() => navigation.navigate('About')}
+                onPress={() => navigation.navigate("Tabs")}
                 activeOpacity={0.6}
                 underlayColor='red'
             >
@@ -33,22 +36,88 @@ function AboutScreen() {
             flex: 1, justifyContent: 'center', alignItems: 'center'
         }}>
             <Text>About!</Text>
+        </View>
+
+
+    );
+}
+function PerformersScreen() {
+    return (
+        <View style={{
+            flex: 1, justifyContent: 'center', alignItems: 'center'
+        }}>
+            <Text>Performers!</Text>
 
         </View>
     );
 }
-const Tab = createBottomTabNavigator();
+function ScheduleScreen() {
+    return (
+        <View style={{
+            flex: 1, justifyContent: 'center', alignItems: 'center'
+        }}>
+            <Text>Schedule!</Text>
+
+        </View>
+    );
+}
+function MapScreen() {
+    return (
+        <View style={{
+            flex: 1, justifyContent: 'center', alignItems: 'center'
+        }}>
+            <Text>Map!</Text>
+
+        </View>
+    );
+}
+function FavoritesScreen() {
+    return (
+        <View style={{
+            flex: 1, justifyContent: 'center', alignItems: 'center'
+        }}>
+            <Text>Favorites!</Text>
+
+        </View>
+    );
+}
+
+function Tabs() {
+    return (
+
+        <Tab.Navigator >
+            <Tab.Screen name="About" component={AboutScreen} />
+            <Tab.Screen name="Performers" component={PerformersScreen} />
+            <Tab.Screen name="Schedule" component={ScheduleScreen} />
+            <Tab.Screen name="Map" component={MapScreen} />
+            <Tab.Screen name="Favorites" component={FavoritesScreen} />
+
+        </Tab.Navigator>
+
+    )
+
+}
+
+
 
 export default function Navigation() {
     return (
         <NavigationContainer>
-            <Stack.Navigator initialRouteName = 'Home'>
-                <Stack.Screen name="Home"
-                    component={HomeScreen} />
-                    <Stack.Screen name="About"
-                    component={AboutScreen} />
+            <Stack.Navigator initialRouteName='Home'>
+                <Stack.Screen name='Home' component={HomeScreen} />
+                <Stack.Screen name = 'Tabs' component={Tabs}/>
             </Stack.Navigator>
         </NavigationContainer>
+
+
+        /*<NavigationContainer>
+            <Stack.Navigator initialRouteName='Home'>
+                <Stack.Screen name="Home"
+                    component={HomeScreen} />
+                <Tab.Screen name="Tabs" component={TabsScreen} />
+            </Stack.Navigator>
+        </NavigationContainer>
+        */
 
     );
 }
